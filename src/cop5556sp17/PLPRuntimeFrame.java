672ab99ce61769f8e6883795b479ef6dd778fa3c
zyn1018 <angelo931018@yahoo.com>
2017-04-18 12:24:11 -0400

    Final Commit

--- src/cop5556sp17/PLPRuntimeFrame.java
+++ src/cop5556sp17/PLPRuntimeFrame.java
@@ -118,7 +118,7 @@ public class PLPRuntimeFrame extends JFrame {
 
 
     private void setImage(final BufferedImage image2) {
-        PLPRuntimeLog.globalLogAddEntry("setImage");
+        PLPRuntimeLog.globalLogAddEntry("showImage");
         try {
             SwingUtilities.invokeAndWait(new Runnable() {
                 public void run() {
